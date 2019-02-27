c73f5c76875dbaaad43565de1cd7e5de9b0c71b1
mattchupp <mattlchupp@gmail.com>
2019-02-26 19:38:05 -0600

    changing some state things

--- src/Weather.js
+++ src/Weather.js
@@ -8,29 +8,34 @@ class Weather extends Component {
   constructor() {
     super();
     this.state = {
+      location: {
+        zipcode: "61821",
+        long: null,
+        lat: null,
+        city: null,
+        state: null
+      },
       temp: "",
       summary: "",
-      long: null,
-      lat: null,
-      city: null,
-      state: null,
     };
+
+
   }
 
   componentDidMount() {
-      axios.get(`https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/JZtnzsSPUV18mGJRsWf5nVG9vngAc8c12VXdaRfqv2xp77q4mSPy6yZWctEDAVAc/info.json/61821/degrees`)
+      axios.get(`https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/JZtnzsSPUV18mGJRsWf5nVG9vngAc8c12VXdaRfqv2xp77q4mSPy6yZWctEDAVAc/info.json/${this.state.location.zipcode}/degrees`)
       .then(res => {
         let presentState = {...this.state};
-          presentState.long = res.data.lng;
-          presentState.lat = res.data.lat;
-          presentState.city = res.data.city;
-          presentState.state = res.data.state;
+          presentState.location.long = res.data.lng;
+          presentState.location.lat = res.data.lat;
+          presentState.location.city = res.data.city;
+          presentState.location.state = res.data.state;
           this.setState({ ...presentState });
         }).catch(err => {
           console.log(err);
         })
     .then(() => {
-      axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/895d852c061ef91db419f40459c25d83/40.116330,-88.243520`)
+      axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/895d852c061ef91db419f40459c25d83/${this.state.location.lat},${this.state.location.long}`)
       .then(res => {
         let presentState = {...this.state};
           presentState.temp = res.data.currently.temperature;
@@ -58,9 +63,10 @@ class Weather extends Component {
       <div style={forecast}>
         <Summary summary={this.state.summary} />
         <Temperature temp={Math.round(this.state.temp)} />
-        <p>Lat: {this.state.lat}</p>
-        <p>Lng: {this.state.long}</p>
-        <p>City: {this.state.city}, {this.state.state}</p>
+        <p>Lat: {this.state.location.lat}</p>
+        <p>Lng: {this.state.location.long}</p>
+        <p>City: {this.state.location.city}, {this.state.location.state}</p>
+        <p>City: {this.state.location.zipcode}</p>
       </div>
     )
   }
