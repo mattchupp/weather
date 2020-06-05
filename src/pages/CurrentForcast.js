a6a806643f4f5020a290111b6a27488be619fc93
mattchupp <mattlchupp@gmail.com>
2020-06-05 10:34:48 -0500

    moving Weather to different component to add more views

--- src/pages/CurrentForcast.js
+++ src/pages/CurrentForcast.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
-import Temperature from './components/Temperature.js';
-import Summary from './components/Summary.js';
-import City from './components/City.js';
+import '../App.css';
+import Temperature from '../components/Temperature.js';
+import Summary from '../components/Summary.js';
+import City from '../components/City.js';
 
 import axios from 'axios';
 
